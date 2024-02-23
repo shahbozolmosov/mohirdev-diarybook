4526bb4dc25fff01331ff7ef275d26a9c122a5ab
shahboz007 <olmasovshahboz007@gmail.com>
2024-02-23 22:31:28 +0500

    start project

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const exphbs = require("express-handlebars");
+
+// Initial env variable
+dotenv.config();
+
+const app = express();
+
+// Initialize template engine (handlebars)
+app.engine(".hbs", exphbs.engine({ extname: ".hbs" }));
+app.set("view engine", ".hbs");
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port: ${PORT}`);
+});
