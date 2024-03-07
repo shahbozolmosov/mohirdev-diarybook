9ea6ceae550f59cf27847595dfccf93d7028142d
shahboz007 <olmasovshahboz007@gmail.com>
2024-03-07 08:34:56 +0500

    added show diary

--- controllers/diary.controller.js
+++ controllers/diary.controller.js
@@ -4,14 +4,24 @@ const Diary = db.diary;
 // Desc     Get all my diaries page
 // Route    GET diary/my
 // Access   Private
-const getMyDiary = (req, res) => {
-  res.render("diary/my-diary");
+const getMyDiary = async (req, res) => {
+  try {
+    const diaries = await Diary.findAll({
+      raw: true
+    });
+    res.render("diary/my-diary", {
+      title: "My Diary",
+      diaries,
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 // Desc     Add new diary
 // Route    POST diary/my
 // Access   Private
-const addNewDiary = async (req, res) => { 
+const addNewDiary = async (req, res) => {
   try {
     const { imageUrl, text } = req.body;
     await Diary.create({
