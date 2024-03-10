28ef93c57cd0084f868e7a3871a2714d9cd811ce
shahboz007 <olmasovshahboz007@gmail.com>
2024-03-10 15:58:21 +0500

    added user model

--- models/index.js
+++ models/index.js
@@ -16,11 +16,37 @@ db.diary = require("./diary.model")(sequelize, Sequelize);
 // comment
 db.comment = require("./comment.model")(sequelize, Sequelize);
 
+// user
+db.user = require("./user.model")(sequelize, Sequelize);
+
 // relationship
+// user
+db.user.hasMany(db.diary, {
+  as: "diaries",
+  onDelete: "CASCADE",
+  constraints: true,
+});
+db.user.hasMany(db.comment, {
+  as: "comment",
+  onDelete: "CASCADE",
+  constraints: true,
+});
+
+// diary
 db.diary.hasMany(db.comment, {
   as: "comment",
   onDelete: "CASCADE",
   constraints: true,
+});
+db.diary.belongsTo(db.user, {
+  foreignKey: "userId",
+  as: "user",
+});
+
+// comment
+db.comment.belongsTo(db.user, {
+  foreignKey: "userId",
+  as: "user",
 });
 db.comment.belongsTo(db.diary, {
   foreignKey: "diaryId",
