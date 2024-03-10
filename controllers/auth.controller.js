77b2ca74b2bd4da22096daf5b94b0664dbd3f2eb
shahboz007 <olmasovshahboz007@gmail.com>
2024-03-10 16:53:44 +0500

    added cookie authenticate

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -6,123 +6,30 @@ const User = db.user;
 // Access   Private
 const getLoginPage = async (req, res) => {
   try {
+    const isAuthenticated = req.get('Cookie').split('=')[1] === "true"
+    console.log(isAuthenticated)
     res.render("auth/login", {
       title: "Login",
+      isAuthenticated
     });
   } catch (error) {
     console.log(error);
   }
 };
 
-// Desc     Get diary by id
-// Route    GET /diary/my
-// Access   Private
-const getDiaryById = async (req, res) => {
+// Desc     Login user
+// Route    POST /auth/login
+// Access   Public
+const loginUser = async (req, res) => {
   try {
-    // const diaries = await Diary.findAll({
-    //   where: {id: req.params.id },
-    //   raw: true
-    // });
-    const data = await Diary.findByPk(req.params.id, {
-      raw: false,
-      plain: true,
-      include: ["comment"],
-      nest: true,
-    });
-
-    const diary = await data.toJSON();
-
-    res.render("diary/one-diary", {
-      title: "Diary",
-      diary: diary,
-      comments: diary.comment.reverse(),
-    });
+    res.setHeader('Set-Cookie', 'loggedIn=true')
+    res.redirect('/diary/my');
   } catch (error) {
     console.log(error);
   }
-};
-
-// Desc     Add new diary
-// Route    POST /diary/my
-// Access   Private
-const addNewDiary = async (req, res) => {
-  try {
-    const { imageUrl, text } = req.body;
-    await Diary.create({
-      imageUrl,
-      text,
-    });
-    res.redirect("/diary/my");
-  } catch (err) {
-    console.log(err);
-  }
-};
-
-// Desc     Update diary page
-// Route    GET /diary/update/:id
-// Access   Private
-const updateDiaryPage = async (req, res) => {
-  try {
-    const diary = await Diary.findByPk(req.params.id, {
-      raw: true,
-    });
-    res.render("diary/update-diary", {
-      title: "Diary",
-      diary: diary,
-    });
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-// Desc     Update diary
-// Route    POST /diary/update/:id
-// Access   Private
-const updateDiary = async (req, res) => {
-  try {
-    await Diary.update(
-      { text: req.body.text },
-      { where: { id: req.params.id } }
-    );
-
-    res.redirect("/diary/my");
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-// Desc     Delete diary
-// Route    POST /diary/delete/:id
-// Access   Private
-const deleteDiary = async (req, res) => {
-  try {
-    await Diary.destroy({
-      where: { id: req.params.id },
-    });
-
-    res.redirect("/diary/my");
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-// Desc     Add comment
-// Route    POST /diary/comment/:id
-// Access   Private
-const addCommentToDiary = async (req, res) => {
-  try {
-    await Comment.create({
-      name: "Username",
-      comment: req.body.comment,
-      diaryId: req.params.id,
-    });
-
-    res.redirect(`/diary/${req.params.id}`);
-  } catch (error) {
-    console.log(error);
-  }
-};
+}; 
 
 module.exports = {
   getLoginPage,
+  loginUser
 };
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,8 +1,9 @@
 const { Router } = require("express");
  
-const { getLoginPage } = require("../controllers/auth.controller");
+const { getLoginPage, loginUser } = require("../controllers/auth.controller");
 const router = Router();
 
 router.get("/login", getLoginPage);
+router.post("/login", loginUser);
 
 module.exports = router;
