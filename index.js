7e0a71b098abc4073ce7b82426a697ad01617614
shahboz007 <olmasovshahboz007>
2024-04-05 22:53:43 +0500

    added home page navigate

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const exphbs = require("express-handlebars");
-const session = require('express-session');
-const pgStore = require('connect-pg-simple')(session)
+const session = require("express-session");
+const pgStore = require("connect-pg-simple")(session);
 const pool = require("./config/db");
 const db = require("./models/index");
 
@@ -13,24 +13,36 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extends: false }));
-app.use(session({
-  store: new pgStore({
-    pool,
-    tableName: 'user_session'
-  }),
-  secret: 'my secret value',
-  resave: false,
-  saveUninitialized: false
-}))
+app.use(
+  session({
+    store: new pgStore({
+      pool,
+      tableName: "user_session",
+    }),
+    secret: "my secret value",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
 // Initialize template engine (handlebars)
 app.engine(".hbs", exphbs.engine({ extname: ".hbs" }));
 app.set("view engine", ".hbs");
 
 // Initialize routes
-app.use("/auth", require('./routes/auth.route'))
+app.use("/auth", require("./routes/auth.route"));
 app.use("/diary", require("./routes/diary.route"));
-app.use("/user", require('./routes/user.route'))
+app.use("/user", require("./routes/user.route"));
+app.use("/", async (req, res) => {
+  try {
+    if (req.session.isLogged) {
+      return res.redirect("/diary/my");
+    }
+    return res.redirect("/auth/login");
+  } catch (error) {
+    console.log(err);
+  }
+});
 
 const PORT = process.env.PORT || 3000;
 
